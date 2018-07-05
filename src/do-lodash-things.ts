6fab687561fdb3f8861ddc48bdf335d28e7f3c48
Alex Turek <alex@convoy.com>
2018-07-05 14:36:53 -0700

    Simplify test case

--- src/do-lodash-things.ts
+++
@@ -1,5 +0,0 @@
-import * as _ from 'lodash';
-
-export async function doLodash(args?: any[]) {
-  return _.isArray(args);
-}
--- test/do-lodash-things.ts
+++ test/do-lodash-things.ts
@@ -1,11 +1,7 @@
-import { doLodash } from '../src/do-lodash-things';
+import 'lodash';
 
-describe(`the exported function`, () => {
-  it(`returns false with no args`, async () => {
-    await expect(doLodash()).resolves.toBe(false);
-  });
-
-  it(`returns true when passed an array`, async () => {
-    await expect(doLodash([1, 2, 'a'])).resolves.toBe(true);
+describe(`lodash`, () => {
+  it(`causes these tests to show open handles`, () => {
+    expect(1).toEqual(1);
   });
 });
